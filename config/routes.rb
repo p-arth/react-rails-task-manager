ab1a40edae19ce7670c3da290491c8614a474d17
Pedro Arthur <pedro.art.vel@gmail.com>
2020-06-30 17:44:25 -0300

    No React custom router

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :tasks
   delete 'tasks', to: 'tasks#delete_done'
-  get 'tasks', to: "application#fallback_index_html", constraints: ->(request) do
-    !request.xhr? && request.format.html?
-  end
+  # get 'tasks', to: "application#fallback_index_html", constraints: ->(request) do
+  #   !request.xhr? && request.format.html?
+  # end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
